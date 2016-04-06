2bd043a7feaa94188affb34a96e21cc1b818e585
Fatih Sen <fatihgsen@gmail.com>
2016-04-06 15:43:57 -0500

    update to vasp conversion

--- diffpy/srfit/fitbase/playing.py
+++ diffpy/srfit/fitbase/playing.py
@@ -8,14 +8,14 @@ import numpy as np
 
 # Creates a VASP input from diffpy structure using ASE
 
-    def vaspcalc(struct,relaxed_struct,energy):
-        ats = Atoms(struct.element, scaled_positions=struct.xyz,cell=struct.lattice.stdbase)
-        write('POSCAR',ats,format='vasp')
+def vaspcalc(struct,relaxed_struct,energy):
+    ats = Atoms(struct.element, scaled_positions=struct.xyz,cell=struct.lattice.stdbase)
+    write('POSCAR',ats,format='vasp')
         
-        ## run VASP here
+    ## run VASP here
         
-        vaspout=read('OUTCAR',format='vasp_out')
-        relaxed_struct=Atoms(struct.element, scaled_positions=vaspout.get_scaled_positions(),cell=vaspout.get_cell())
-        energy=vaspout.get_potential_energy()
+    vaspout=read('OUTCAR',format='vasp_out')
+    relaxed_struct=Atoms(struct.element, scaled_positions=vaspout.get_scaled_positions(),cell=vaspout.get_cell())
+    energy=vaspout.get_potential_energy()
         
-        return
+    sreturn
